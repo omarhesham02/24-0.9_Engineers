a844a422e2a5e32b26db0086fbb06be0a39293b7
OmarT123 <94041039+OmarT123@users.noreply.github.com>
2025-03-03 22:34:00 +0200

    Added Cart function headers

--- src/main/java/com/example/controller/CartController.java
+++ src/main/java/com/example/controller/CartController.java
@@ -1,4 +1,39 @@
 package com.example.controller;
 
+import com.example.model.Cart;
+import com.example.model.Product;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/cart")
 public class CartController {
+
+    @PostMapping("/")
+    public Cart addCart(@RequestBody Cart cart){
+        return null;
+    }
+
+    @GetMapping("/")
+    public ArrayList<Cart> getCarts(){
+        return null;
+    }
+
+    @GetMapping("/{cartId}")
+    public Cart getCartById(@PathVariable UUID cartId){
+        return null;
+    }
+
+    @PutMapping("/addProduct/{cartId}")
+    public String addProductToCart(@PathVariable UUID cartId, @RequestBody Product product){
+        return null;
+    }
+
+    @DeleteMapping("/delete/{cartId}")
+    public String deleteCartById(@PathVariable UUID cartId){
+        return null;
+    }
+
 }
--- src/main/java/com/example/repository/CartRepository.java
+++ src/main/java/com/example/repository/CartRepository.java
@@ -1,4 +1,54 @@
 package com.example.repository;
 
-public class CartRepository {
+import com.example.model.Cart;
+import com.example.model.Product;
+import com.example.model.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+@Repository
+@SuppressWarnings("rawtypes")
+public class CartRepository extends MainRepository<Cart> {
+
+    public CartRepository() {}
+
+    @Override
+    protected String getDataPath() {
+        return "src/main/java/com/example/data/carts.json";
+    }
+
+    @Override
+    protected Class<Cart[]> getArrayType() {
+        return Cart[].class;
+    }
+
+    public Cart addCart(Cart cart){
+        return null;
+    }
+
+    public ArrayList<Cart> getCarts(){
+        return null;
+    }
+
+    public Cart getCartById(UUID id){
+        return null;
+    }
+
+    public Cart getCartByUserId(UUID userId){
+        return null;
+    }
+
+    public void addProductToCart(UUID cartId, Product product){
+
+    }
+
+    public void deleteProductFromCart(UUID cartId, Product product){
+
+    }
+
+    public void deleteCartById(UUID cartId){
+
+    }
 }
