e8a73f7208df99c314ba2fc9427a9bfe391c43c1
Hosain Ghoraba <hussain.ghoraba@gmail.com>
2025-03-05 06:31:52 +0200

    initial order structure done to be able to test testing :)

--- src/main/java/com/example/repository/OrderRepository.java
+++ src/main/java/com/example/repository/OrderRepository.java
@@ -1,4 +1,41 @@
 package com.example.repository;
 
-public class OrderRepository {
+import com.example.model.Order;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+@Repository
+@SuppressWarnings("rawtypes")
+public class OrderRepository extends MainRepository<Order> {
+
+    public OrderRepository() {
+    }
+
+    @Override
+    protected String getDataPath() {
+        return "";
+    }
+    @Override
+    protected Class<Order[]> getArrayType() {
+        return null;
+    }
+
+
+    public void addOrder(Order order){
+
+    }
+
+    public ArrayList<Order> getOrders(){
+        return null;
+    }
+
+    public Order getOrderById(UUID orderId){
+        return null;
+    }
+
+    public void deleteOrderById(UUID orderId){
+
+    }
 }
