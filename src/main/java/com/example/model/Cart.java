bf56ff1871d0e7f1bfbe08045ab00cdee293e832
MoTammaa <mohamed.abotammaa@student.guc.edu.eg>
2025-03-06 16:54:29 +0200

    Models constructors, getters & setters

--- src/main/java/com/example/model/Cart.java
+++ src/main/java/com/example/model/Cart.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Component
@@ -11,4 +12,67 @@ public class Cart {
     private UUID id;
     private UUID userId;
     private List<Product> products = new ArrayList<>();
+
+    public Cart() {}
+
+    public Cart(UUID id, UUID userId, List<Product> products) {
+        this.id = id;
+        this.userId = userId;
+        this.products = products;
+    }
+
+    public Cart(UUID userId, List<Product> products) {
+        this.id = UUID.randomUUID();
+        this.userId = userId;
+        this.products = products;
+    }
+
+    public Cart(UUID userId) {
+        this.id = UUID.randomUUID();
+        this.userId = userId;
+        this.products = new ArrayList<>();
+    }
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    @Override
+    public String toString() {
+        return "Cart %s of User(%s) { %s }".formatted(id, userId, products);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cart cart)) return false;
+        return id.equals(cart.id) && userId.equals(cart.userId) && products.equals(cart.products);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userId, products);
+    }
+
 }
