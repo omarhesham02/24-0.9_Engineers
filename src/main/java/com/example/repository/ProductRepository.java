8402fb3d31941f749f7b25c33b788be360b26341
OmarAhmedAdel <omaradel2602@gmail.com>
2025-03-06 07:20:59 +0200

    Added Initial Function Headers!

--- src/main/java/com/example/repository/ProductRepository.java
+++ src/main/java/com/example/repository/ProductRepository.java
@@ -1,4 +1,45 @@
 package com.example.repository;
 
-public class ProductRepository {
+import com.example.model.Product;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+@Repository
+@SuppressWarnings("rawtypes")
+public class ProductRepository extends MainRepository<Product> {
+
+    public ProductRepository() {
+    }
+
+    @Override
+    protected String getDataPath() {
+        return "src/main/java/com/example/data/products.json";
+    }
+
+    @Override
+    protected Class<Product[]> getArrayType() {
+        return null;
+    }
+
+
+    public Product addProduct(Product product){
+        return null;
+    }
+    public ArrayList<Product> getProducts(){
+        return null;
+    }
+    public Product getProductById(UUID productId){
+        return null;
+    }
+    public Product updateProduct(UUID productId, String newName, double newPrice) {
+        return null;
+    }
+    public void applyDiscount(double discount, ArrayList<UUID> productIds){
+
+    }
+    public void deleteProductById(UUID productId){
+
+    }
 }
